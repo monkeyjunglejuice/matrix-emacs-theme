ebe268edad19d25227f2cdaa5a55ca0378cdad53
Dan Dee <monkeyjunglejuice@pm.me>
2022-01-09 13:54:38 +0800

    File display in Git

--- the-matrix-theme.el
+++ the-matrix-theme.el
@@ -265,7 +265,7 @@
    `(git-commit-comment-heading ((t (:slant italic :inherit font-lock-comment-face))))
    `(git-commit-comment-branch-local ((t (:slant italic :weight bold))))
    `(git-commit-comment-branch-remote ((t (:slant italic :weight bold))))
-   `(git-commit-comment-file ((t (:inherit font-lock-string-face))))
+   `(git-commit-comment-file ((t (:foreground ,color-bright :background ,color-bg-alt))))
    `(git-commit-comment-action ((t (:weight bold :inherit font-lock-comment-face))))
 
    ;; Magit
