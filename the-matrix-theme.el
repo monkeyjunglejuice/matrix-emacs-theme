3b598b4ad25d944a34c66faa6748477a52ad2837
Dan Dee <monkeyjunglejuice@pm.me>
2024-04-15 07:44:27 +0800

    Make headlines stand out

--- the-matrix-theme.el
+++ the-matrix-theme.el
@@ -228,14 +228,14 @@ You'll have to see it for yourself. --Morpheus")
    `(font-latex-script-char-face ((t (:inherit font-latex-math-face))))
 
    ;; outline
-   `(outline-1 ((t (:foreground ,color-fg :weight bold :height 1.2))))
-   `(outline-2 ((t (:foreground ,color-fg :weight bold))))
-   `(outline-3 ((t (:foreground ,color-fg :weight bold))))
-   `(outline-4 ((t (:foreground ,color-fg :weight bold))))
-   `(outline-5 ((t (:foreground ,color-fg :weight bold))))
-   `(outline-6 ((t (:foreground ,color-fg :weight bold))))
-   `(outline-7 ((t (:foreground ,color-fg :weight bold))))
-   `(outline-8 ((t (:foreground ,color-fg :weight bold))))
+   `(outline-1 ((t (:foreground ,color-bright :weight bold :height 1.2))))
+   `(outline-2 ((t (:foreground ,color-middle :weight bold))))
+   `(outline-3 ((t (:foreground ,color-middle :weight bold))))
+   `(outline-4 ((t (:foreground ,color-middle :weight bold))))
+   `(outline-5 ((t (:foreground ,color-middle :weight bold))))
+   `(outline-6 ((t (:foreground ,color-middle :weight bold))))
+   `(outline-7 ((t (:foreground ,color-middle :weight bold))))
+   `(outline-8 ((t (:foreground ,color-middle :weight bold))))
 
    ;; org-mode
    `(org-archived ((t (:foreground ,color-dark))))
