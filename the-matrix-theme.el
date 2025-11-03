ff0d3ba077d7d48c46a00b724de8eb4ce163fab9
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-11-03 18:21:57 +0800

    v1.4.2

--- the-matrix-theme.el
+++ the-matrix-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Dan Dee <monkeyjunglejuice@pm.me>
 ;; URL: https://github.com/monkeyjunglejuice/matrix-emacs-theme
-;; Version: 1.4.1
+;; Version: 1.4.2
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme
 ;; SPDX-License-Identifier: MIT
